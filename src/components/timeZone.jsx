419cd6e3ea54ece37aad19c221ffcf960a4ea9e9
Andy Chau <>
2020-09-15 18:56:06 +1000

    Stage1 Reformat timeZone.jsx

--- src/components/timeZone.jsx
+++ src/components/timeZone.jsx
@@ -3,7 +3,7 @@ import { getZones } from "../services/mockTimezone";
 
 class TimeZones extends Component {
   state = {
-      zones : getZones()
+    zones: getZones(),
   };
 
   render() {
@@ -18,25 +18,28 @@ class TimeZones extends Component {
           </tr>
         </thead>
         <tbody className="tr">
-            {this.state.zones.map(tz => 
-                      <tr key={tz.zoneName}>
-                      <td>{tz.countryName}</td>
-                      <td>{tz.zoneName}</td>
-                      <td>{tz.gmtOffset}</td>
-                      <td>{this.formatDate(tz.timestamp)}</td>
-                    </tr>
-                )}
-
+          {this.state.zones.map((tz) => (
+            <tr key={tz.zoneName}>
+              <td>{tz.countryName}</td>
+              <td>{tz.zoneName}</td>
+              <td>{tz.gmtOffset}</td>
+              <td>{this.formatDate(tz.timestamp)}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     );
   }
 
   formatDate = (ts) => {
-      const d = new Date(ts);
-      const tf = new Intl.DateTimeFormat('en-AU', {hour: 'numeric', minute: 'numeric', second:'numeric'})
-      return tf.format(d);
-  }
+    const d = new Date(ts);
+    const tf = new Intl.DateTimeFormat("en-AU", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    return tf.format(d);
+  };
 }
 
 export default TimeZones;
