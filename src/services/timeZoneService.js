a775c73186f1c038d339cc60a316061d1661a0a9
Andy Chau <>
2020-09-16 12:03:45 +1000

    Stage1 Fix rst.length bug, add checking only print rst is valid object.

--- src/services/timeZoneService.js
+++ src/services/timeZoneService.js
@@ -3,6 +3,10 @@ import axios from "axios";
 
 export async function getTimeZoneList() {
   const { data: rst } = await axios.get(timeZoneListUrl);
-  console.log(`Timezone list fetch completed lenght: ${rst.length()}`);
+  if (rst) {
+    console.log(`Timezone list fetch completed lenght: ${rst.zones.length}`);
+  } else {
+    console.log("Timezone list not able to return!");
+  }
   return rst.zones;
 }
